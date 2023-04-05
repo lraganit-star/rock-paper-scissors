3d610faeff1536eeca8a996edd23083959ea6935
Leslie Raganit <niki@Leslies-MacBook-Pro.local>
2023-04-05 13:17:44 -0700

    added mechanics of who wins the game

--- rules.js
+++ rules.js
@@ -4,15 +4,25 @@ function getComputerChoice() {
     return choices[random]
 }
 
-function letsPlay(playerSelection = "rOck", computerSelection = getComputerChoice()) {
+function letsPlay(playerSelection = "scissors", computerSelection = getComputerChoice()) {
     const userChoice = playerSelection.toLowerCase()
+    console.log(computerSelection)
     const choices = [['rock','scissors'],
                         ['scissors', 'paper'],
                         ['paper', 'rock']]
+    
     if (userChoice === computerSelection) {
         return "It's a tie"
     }
-    return "Not a tie"
+
+    for (i = 0; i < choices.length; i++) {
+        console.log(choices[i])
+        if (userChoice == choices[i][0] && computerSelection != choices[i][1]) {
+            return `You Lose! ${computerSelection} beats ${userChoice}`
+            
+        }
+    }
+    return `You win! ${userChoice} beats ${computerSelection}`
 }
 
 console.log(letsPlay())
